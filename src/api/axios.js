22eb038e7abad719c5ac1f6acd5c35c24df5da60
pastuchenko <pablopastuchenkooo@gmail.com>
2024-10-22 11:08:47 -0300

    first commit

---
+++ src/api/axios.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const baseURL = "https://api.edamam.com/api/recipes/v2";
+
+function fetchData(url) {
+  const instance = axios.create();
+  
+  if (url.startsWith("http://") || url.startsWith("https://")) {
+    return instance.get(url); 
+  } else {
+    
+    instance.defaults.baseURL = baseURL;
+    return instance.get(url);
+  }
+}
+
+export default fetchData;
